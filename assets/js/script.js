6152a145a5d1ac281fddedbf55c0703d1b11911c
davy-berry <davy.berry@yahoo.co.uk>
2026-07-27 00:12:34 +0100

    Display operator in question area

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,19 +9,27 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("Submit button clicked!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You Selected: ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+    runGame("addition");
 });
 
 /**
  * Runs the game based on the selected game type  and afeter the user has submitted an answer
  */
-function runGame() {
+function runGame(gameType) {
     // Create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -36,7 +44,10 @@ function incrementScore() {
 function incrementWrongAnswer() {
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+   document.getElementById("operand1").textContent = operand1;
+   document.getElementById("operand2").textContent = operand2;
+   document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion() {
