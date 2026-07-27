0aaa3135fe5801b268b0525087567ade32e370b1
davy-berry <davy.berry@yahoo.co.uk>
2026-07-27 01:21:48 +0100

    Add displaySubtractQuestion

--- assets/js/script.js
+++ assets/js/script.js
@@ -102,7 +102,11 @@ function displayAdditionQuestion(operand1, operand2) {
    document.getElementById("operator").textContent = "+";
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(oeprand1, operand2) {
+
+    document.getElementById("operand1").textContent = oeprand1 > operand2 ? oeprand1 : operand2;
+    document.getElementById("operand2").textContent = oeprand1 > operand2 ? operand2 : oeprand1;
+    document.getElementById("operator").textContent = "-";
 }  
 
 function displayMultiplyQuestion(operand1, operand2) {
